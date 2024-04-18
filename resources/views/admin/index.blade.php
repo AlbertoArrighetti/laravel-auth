2fe32bf0d5cd4821d441e051e53eff5fcfbea461
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-04-18 17:29:50 +0200

    feat: projects index page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,8 +2,14 @@
 
 @section('content')
     <div class="container py-5">
-        <h1>Admin Page</h1>
+        <h1 class="text-center mb-5">Admin Page</h1>
 
-        <h2>Welcome {{$user->name}}</h2>
+        <h4 class="pb-5 ">Welcome {{$user->name}}</h4>
+
+        <div>
+            View all the projects
+            <a href="{{route('projects.index')}}">here</a>
+        </div>
+
     </div>
 @endsection
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts/app')
 
 @section('content')
-
+<div class="container py-5 ">
+    <h1 class="mb-3">Projects List</h1>
+    
+    <table class="table mb-5 ">
+        <thead>
+            <tr>
+                <th scope="col" class="pb-4">Title</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+            <tr>
+                
+                <td>{{$project->title}}</td>
+                
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
